fbfd64f72cc5614d5073fcdfc838f920609977f6
shahriar fatthi <alifattahisf@gmail.com>
2023-11-20 15:18:16 +0330

    craete User, UserAddress Model

---
+++ src/users/managers.py
@@ -0,0 +1,23 @@
+from django.contrib.auth.models import BaseUserManager
+
+
+class UserManager(BaseUserManager):
+    def create_user(self, phone, email, password):
+        if not email:
+            raise ValueError('Email must be entered')
+        if not phone:
+            raise ValueError('Phone must be entered')
+        user = self.model(
+            email=self.normalize_email(email),
+            phone=phone
+        )
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+    
+    def create_superuser(self, phone, email, password):
+        user = self.create_user(phone, email, password)
+        user.is_admin = True
+        user.is_superuser = True
+        user.save(using= self._db)
+        return user
--- src/users/models.py
+++ src/users/models.py
@@ -1,3 +1,54 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
+from .managers import UserManager
 
-# Create your models here.
+class User(AbstractBaseUser, PermissionsMixin):
+    class Meta:
+        ordering = ['-id']
+    
+    #identifier
+    email = models.EmailField(max_length=254, unique=True)
+
+    phone = models.CharField(max_length=11, unique=True)
+    first_name = models.CharField(max_length=50, blank=True, null=True)
+    last_name = models.CharField(max_length=50, blank=True, null=True)
+    email_verified = models.BooleanField(default=False)
+    profile_picture = models.ImageField(upload_to='profiles', blank=True, null=True)
+
+    is_active = models.BooleanField(default=True)
+    is_admin = models.BooleanField(default=False)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['phone',]
+
+    objects = UserManager()
+
+    @property
+    def is_staff(self):
+        return self.is_admin
+    def has_perm(self,perm,obj=None):
+        return True
+    def has_module_perms(self,app_label):
+        return True
+    def __str__(self) -> str:
+        return self.email
+
+
+class UserAddress(models.Model):
+    class Meta:
+        ordering = ['-id']
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name='addresses')
+    first_name = models.CharField(max_length=20)
+    last_name = models.CharField(max_length=30)
+    phone_number = models.CharField(max_length=11)
+    country = models.CharField(max_length=50, default='Iran')
+    province = models.CharField(max_length=50)
+    city = models.CharField(max_length=50)
+    details = models.TextField()
+    house_number = models.CharField(max_length=20)
+    unit = models.IntegerField(null=True, blank=True)
+    postal_code = models.IntegerField()
+
+    def __str__(self) -> str:
+        return f'{self.country} - {self.province} - {self.city}'
+    
