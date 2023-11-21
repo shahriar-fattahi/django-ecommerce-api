041385f572431eba063116e7d55fc59277844b59
shahriar fatthi <alifattahisf@gmail.com>
2023-11-21 22:16:03 +0330

    Project settings were modified

--- src/manage.py
+++ src/manage.py
@@ -2,11 +2,14 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
-
+from src.settings.base import DEBUG
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'src.settings')
+    if DEBUG:
+        os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'src.settings.local')
+    else:
+        os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'src.settings.production')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
--- src/src/urls.py
+++ src/src/urls.py
@@ -1,7 +1,27 @@
 from django.contrib import admin
 from django.urls import path, include
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+from rest_framework import permissions
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="E-commerce API",
+      default_version='1.0.0',
+      description="Django Rest Framework E-commerce API",
+      contact=openapi.Contact(email="alifattahisf@gmail.com"),
+   ),
+   public=True,
+   permission_classes=(permissions.AllowAny,),
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('users/', include('users.urls'))
+    path("api/v1/",
+         include([
+             path('users/', include('users.urls', namespace='users')),
+             path('swagger/schema/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger'),
+         ])
+         ),
+    
 ]
