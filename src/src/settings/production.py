24bd0972beff924ffa236e36a6c0741e8fb1fc0c
shahriar fatthi <alifattahisf@gmail.com>
2023-11-22 22:54:28 +0330

    production settings modified

--- src/src/settings/production.py
+++ src/src/settings/production.py
@@ -1,3 +1,19 @@
 from .base import *
 
 ALLOWED_HOSTS = ["*"]
+
+PNAME = os.environ.get("PNAME")
+PUSER = os.environ.get("PUSER")
+PPASSWORD = os.environ.get("PPASSWORD")
+PHOST = os.environ.get("PHOST")
+PPORT = os.environ.get("PPORT")
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "NAME": PNAME,
+        "USER": PUSER,
+        "PASSWORD": PPASSWORD,
+        "HOST": PHOST,
+        "PORT": PPORT,
+    }
+}
